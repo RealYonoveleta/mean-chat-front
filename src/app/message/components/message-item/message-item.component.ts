77ab339ce5fe6c22779b6ecefa9f12001092cac0
Jonas <martinezcuestajonas@gmail.com>
2026-03-03 23:51:06 +0100

    chore(message-item): add remaining files to previous commit -oops-

--- src/app/message/components/message-item/message-item.component.ts
+++ src/app/message/components/message-item/message-item.component.ts
@@ -1,14 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, computed, inject, input, OnInit, signal } from '@angular/core';
+import { Message } from '../../../model/message';
+import { User } from '../../../model/user';
+import { UserService } from '../../../user/services/user.service';
 
 @Component({
   selector: 'app-message-item',
   templateUrl: './message-item.component.html',
   styleUrls: ['./message-item.component.scss'],
+  host: {
+    '[class]': "isSelf() ? 'self' : 'other'",
+    '[class.grouped]': 'isGrouped()',
+    '[class.first]': 'isFirstOfGroup()',
+  },
 })
-export class MessageItemComponent  implements OnInit {
+export class MessageItemComponent implements OnInit {
+  message = input.required<Message>();
+  isFirstOfGroup = input<boolean>();
+  isGrouped = input<boolean>();
+  isSelf = input<boolean>();
 
-  constructor() { }
+  private userService = inject(UserService);
 
-  ngOnInit() {}
+  user = signal<User | null>(null);
+  userDisplayName = computed(() => `${this.user()?.name} ${this.user()?.surname}`);
 
+  ngOnInit() {
+    this.userService.getUser(this.message().senderId).subscribe((user) => this.user.set(user));
+  }
 }
--- src/app/message/components/message-list/message-list.component.ts
+++ src/app/message/components/message-list/message-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, inject, input } from '@angular/core';
-import { Message } from '../../../model/message';
-import { CurrentUserService } from '../../../core/user/current-user.service';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { CurrentUserService } from '../../../core/user/current-user.service';
+import { Message } from '../../../model/message';
+import { MessageItemComponent } from '../message-item/message-item.component';
 
 @Component({
   selector: 'app-message-list',
   templateUrl: './message-list.component.html',
   styleUrls: ['./message-list.component.scss'],
+  imports: [MessageItemComponent],
 })
 export class MessageListComponent {
   messages = input<Message[]>();
+
   private currentUserService = inject(CurrentUserService);
 
   user = toSignal(this.currentUserService.currentUser$);
@@ -22,5 +25,15 @@ export class MessageListComponent {
     if (!next) return false;
 
     return current.senderId === next.senderId;
+  }
+
+  isFirstOfGroup(index: number): boolean {
+    const messages = this.messages()!;
+    const current = messages[index];
+    const previous = messages[index - 1];
+
+    if (!previous) return true;
+
+    return current.senderId !== previous.senderId;
   }
 }
